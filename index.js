d58123e4da067cf5955758ec5384e455d1084796
V1nic1o <evaldezc5@miumg.edu.gt>
2025-05-16 18:24:10 -0600

    🛠️ Aumentado límite de carga para aceptar logos en base64

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
+// Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 
 // Rutas
 const companyRoutes = require('./routes/company.routes');
